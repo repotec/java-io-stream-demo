4a458e2c8610cecb35b0ff6cb4a00a167f2a9004
repotec <repotecing@gmail.com>
2022-10-03 21:12:13 +0300

    add InputStreamReader demos

---
+++ src/com/stream/demo/characters/reader/InputStreamReaderDemo.java
@@ -0,0 +1,33 @@
+package com.stream.demo.characters.reader;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Properties;
+
+public class InputStreamReaderDemo {
+	public static void main(String[] args) {
+		read("test.properties");
+	}
+	
+	
+	
+	public static void read(String filePath) {
+		Properties props = new Properties();
+		
+		try(InputStream in = new FileInputStream(filePath);
+			InputStreamReader reader = new InputStreamReader(in, Charset.forName("UTF-8"));
+			FileReader file = new FileReader(filePath)){
+			
+			props.load(file);
+			
+			props.entrySet().stream().forEach(e -> System.out.println(e));
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
